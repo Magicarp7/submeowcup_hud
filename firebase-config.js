51fb4c01b8c59c6eea9229e7c7c0ac4595f0e85a
Magicarp7 <163570816+Magicarp7@users.noreply.github.com>
2026-07-02 21:33:58 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAqmC_LOsxLhU1CXGfClBL4ixtV0VPjna8",
+  authDomain: "valorant-hud-c52e2.firebaseapp.com",
+  databaseURL: "https://valorant-hud-c52e2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "valorant-hud-c52e2",
+  storageBucket: "valorant-hud-c52e2.firebasestorage.app",
+  messagingSenderId: "451334454270",
+  appId: "1:451334454270:web:9b1326ff890be87b8f5eaf"
+};
+
+window.HUD_PATH = "hud";
